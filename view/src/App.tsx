340b16d53e188ce0dcab6f283a43c49cfd09c1fd
ferueda <fe.rueda@outlook.com>
2022-01-12 21:06:04 -0800

    Create main App component

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,41 +1,16 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import Chat from './Chat';
 
 function App() {
-  const [input, setInput] = React.useState('');
-  const [socket, setSocket] = React.useState<WebSocket | null>(null);
-  const [messages, setMessages] = React.useState<string[]>([]);
-
-  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInput(e.target.value);
-  };
-
-  const submitForm = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (input === '' || !socket) return;
-    console.log('caca');
-    setInput('');
-    socket.send(input);
-  };
-
-  React.useEffect(() => {
-    if (!window['WebSocket'] || socket) return;
-    const s = new WebSocket('ws://localhost:8080/room');
-    s.onmessage = (e) => setMessages((prev) => prev.concat(e.data));
-    setSocket(s);
-  }, [socket]);
-
   return (
     <div className="App">
       <h1>React-go Chat app</h1>
-      <ul id="messages" style={{ listStyle: 'none', paddingLeft: 0 }}>
-        {messages.map((m) => {
-          return <li key={m}>{m}</li>;
-        })}
-      </ul>
-      <form id="chatbox" onSubmit={submitForm}>
-        <textarea onChange={onChange} style={{ display: 'block' }} />
-        <button type="submit">Send</button>
-      </form>
+
+      <Routes>
+        <Route path="/" element={<Chat />} />
+      </Routes>
     </div>
   );
 }
