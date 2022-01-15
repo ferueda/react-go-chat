8131fa15430de9103ea91fa461d7fb27ddb207b7
ferueda <fe.rueda@outlook.com>
2022-01-14 20:01:24 -0800

    Update Chat component to receive who is online messages from the server

--- view/src/components/Chat.tsx
+++ view/src/components/Chat.tsx
@@ -3,22 +3,35 @@ import React from 'react';
 import LoginForm from './LoginForm';
 import TextAreaInput from './shared/TextAreaInput';
 import Button from './shared/Button';
+import Avatar from './shared/Avatar';
+import UserList from './UserList';
 
 export default function Chat() {
   const [input, setInput] = React.useState('');
-  const [socket, setSocket] = React.useState<WebSocket | null>(null);
+  const [socket, setSocket] = React.useState<WebSocket>();
   const [messages, setMessages] = React.useState<
     { username: string; message: string; when: string; avatar?: string }[]
   >([]);
-  const [token, setToken] = React.useState<string | null>(null);
-  const [username, setUsername] = React.useState<string | null>(null);
+  const [token, setToken] = React.useState<string>();
+  const [username, setUsername] = React.useState<string>();
+  const [users, setUsers] = React.useState<string[]>([]);
 
   React.useEffect(() => {
     if (!window['WebSocket'] || socket) return;
     if (!token || !username) return;
 
     const s = new WebSocket(`ws://localhost:8080/room?t=${token}`);
-    s.onmessage = (e) => setMessages((prev) => prev.concat(JSON.parse(e.data)));
+    s.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+
+      if (data.username) {
+        setMessages((prev) => prev.concat(JSON.parse(e.data)));
+      }
+
+      if (data.users) {
+        setUsers(data.users);
+      }
+    };
     setSocket(s);
   }, [socket, token, username]);
 
@@ -45,42 +58,58 @@ export default function Chat() {
   };
 
   return (
-    <div className="mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-      {token && username ? (
-        <div className="w-96 relative mx-auto bg-white rounded-md px-10 py-4">
-          <h3 className="absolute top-2 font-bold">
-            <span className="font-light">Logged in as </span>
-            {username}
-          </h3>
-          <div id="messages" className="h-80 bg-gray-100 mb-4 mt-6 p-2 rounded-md overflow-auto">
-            {messages.map((m, i, a) => {
-              return (
-                <div key={m.when + ' - ' + m.username} className="my-1">
-                  {m.username !== username && i > 0 && a[i - 1].username !== m.username && (
-                    <div className="font-bold">{m.username}</div>
-                  )}
-                  <div className={`overflow-x-auto ${m.username === username && 'text-right'}`}>
-                    {m.message}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <form id="chatbox" className="flex flex-col" onSubmit={submitForm}>
-            <TextAreaInput
-              name="message"
-              value={input}
-              placeholder="Type a message"
-              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setInput(e.target.value)}
-            />
-            <Button type="submit" className="mt-3">
-              Send
-            </Button>
-          </form>
+    <div className="relative">
+      {token && username && (
+        <div className="absolute left-0 top-12 lg:top-16">
+          <UserList users={users} />
         </div>
-      ) : (
-        <LoginForm onSubmit={login} />
       )}
+      <div className="mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        {token && username ? (
+          <div className="w-2/3 relative mx-auto bg-white rounded-md px-10 py-4">
+            <h3 className="absolute top-2 font-bold">
+              <span className="font-light">Logged in as </span>
+              {username}
+            </h3>
+            <div
+              id="messages"
+              className="h-96 bg-blue-50 mb-4 mt-6 py-2 px-4 rounded-md overflow-auto"
+            >
+              {messages.map((m, i, a) => {
+                return (
+                  <div key={m.when + ' - ' + m.username}>
+                    {m.username !== username &&
+                      ((i > 0 && a[i - 1].username !== m.username) || i === 0) && (
+                        <div className="flex items-center my-2">
+                          <Avatar src={m.avatar} alt={m.username + ' avatar'} />
+                          <div className="font-bold">{m.username}</div>
+                        </div>
+                      )}
+                    <div
+                      className={`overflow-x-auto ml-2 ${m.username === username && 'text-right'}`}
+                    >
+                      {m.message}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <form id="chatbox" className="flex flex-col" onSubmit={submitForm}>
+              <TextAreaInput
+                name="message"
+                value={input}
+                placeholder="Type a message"
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setInput(e.target.value)}
+              />
+              <Button type="submit" className="mt-3">
+                Send
+              </Button>
+            </form>
+          </div>
+        ) : (
+          <LoginForm onSubmit={login} />
+        )}
+      </div>
     </div>
   );
 }
