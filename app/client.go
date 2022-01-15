b3a5ab891b29d3b3d51e9ba147547b63ec692809
ferueda <fe.rueda@outlook.com>
2022-01-14 20:00:33 -0800

    Add online users to client implementation

--- app/client.go
+++ app/client.go
@@ -10,18 +10,26 @@ type client struct {
 	socket *websocket.Conn
 	// send is a channel on which messages are sent.
 	send chan *message
+	// online is a channel on which online users list is sent.
+	online chan []string
 	// room is the room this client is chatting in
 	room *room
 	// user holds information about the user
-	user map[string]interface{}
+	user *user
 }
 
-func newClient(s *websocket.Conn, r *room, username string) *client {
+type user struct {
+	username string
+	avatar   string
+}
+
+func newClient(s *websocket.Conn, r *room, u *user) *client {
 	return &client{
 		socket: s,
 		send:   make(chan *message),
+		online: make(chan []string),
 		room:   r,
-		user:   map[string]interface{}{"username": username},
+		user:   u,
 	}
 }
 
@@ -35,9 +43,9 @@ func (c *client) read() {
 		}
 
 		msg.When = time.Now()
-		msg.Username = c.user["username"].(string)
-		if avatar, ok := c.user["avatar"]; ok {
-			msg.Avatar = avatar.(string)
+		msg.Username = c.user.username
+		if c.user.avatar != "" {
+			msg.Avatar = c.user.avatar
 		}
 		c.room.forward <- msg
 	}
@@ -47,6 +55,19 @@ func (c *client) write() {
 	defer c.socket.Close()
 	for msg := range c.send {
 		err := c.socket.WriteJSON(msg)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (c *client) newUser() {
+	defer c.socket.Close()
+	for msg := range c.online {
+		err := c.socket.WriteJSON(struct {
+			Users []string `json:"users"`
+		}{Users: msg})
+
 		if err != nil {
 			return
 		}
